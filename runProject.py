fcfcce269234813ada57776fd7fe221d9cb071bd
Shashwat Sehgal <shashwatsehgal@gmail.com>
2017-09-27 20:33:02 -0700

    Dashboard complete

--- runProject.py
+++ runProject.py
@@ -30,11 +30,11 @@ class RunProject(webapp2.RequestHandler):
 
 		api = finding(appid='Shashwat-ToLoP-PRD-35d80d3bd-64e84449', config_file=None)
 		api.execute('findItemsAdvanced', {
-			'keywords': ['toys r us','exclusive','star wars'],
+			'keywords': ['toys r us','exclusive',existingProject.projectName],
 			'itemFilter': [
-        			{'name': 'Condition', 'value': 'Used'},
-        			{'name': 'MinPrice', 'value': '11', 'paramName': 'Currency', 'paramValue': 'USD'},
-        			{'name': 'MaxPrice', 'value': '11', 'paramName': 'Currency', 'paramValue': 'USD'}
+        			{'name': 'Condition', 'value': 'New'},
+        			{'name': 'MinPrice', 'value': '1', 'paramName': 'Currency', 'paramValue': 'USD'},
+        			{'name': 'MaxPrice', 'value': '10000', 'paramName': 'Currency', 'paramValue': 'USD'}
     			],
     			'paginationInput': {
         			'entriesPerPage': '25',
@@ -42,21 +42,31 @@ class RunProject(webapp2.RequestHandler):
 			},
     			'sortOrder': 'CurrentPriceHighest'
 		})
-
-dictstr = api.response.reply.searchResult
-
-for i in dictstr.item:
-    print "Title: %s" % i.title
-    print "URL: %s" % i.viewItemURL
-    print "ZIP Code: %s" % i.postalCode
-    print "Location: %s" % i.location
-    print "Price: $%s" % i.sellingStatus.currentPrice.value
-    print "Condition: %s" % i.condition.conditionDisplayName
-    print "Post Time: %s" % i.listingInfo.startTime
-    if hasattr(i.shippingInfo, 'shippingServiceCost'): 
-    	print "Shipping Cost: $%s" % i.shippingInfo.shippingServiceCost.value 
-    else: 
-    	print "Shipping Cost: $0"
-    print "\n"
-#    print "CategoryID: %s" % item['primaryCategory']['categoryId'].value
+		
+		dictstr = api.response.reply.searchResult
+ 		
+		template_values = {
+			'greeting': 'Results',
+                        'url2': ('/dashboard'),
+                        'button1': 'Save Results',
+                        'button2': 'Return to Dashboard',
+                        'searchResults': dictstr 
+                        }
+                template = JINJA_ENVIRONMENT.get_template('www/results.html')
+                self.response.write(template.render(template_values))		
+	
+	
+		for i in dictstr.item:
+    			print "Title: %s" % i.title
+    			print "URL: %s" % i.viewItemURL
+    			print "ZIP Code: %s" % i.postalCode
+    			print "Location: %s" % i.location
+    			print "Price: $%s" % i.sellingStatus.currentPrice.value
+    			print "Condition: %s" % i.condition.conditionDisplayName
+    			print "Post Time: %s" % i.listingInfo.startTime
+    			if hasattr(i.shippingInfo, 'shippingServiceCost'): 
+    				print "Shipping Cost: $%s" % i.shippingInfo.shippingServiceCost.value 
+    			else: 
+    				print "Shipping Cost: $0"
+    				print "\n"
 

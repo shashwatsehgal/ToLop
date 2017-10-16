8c53c73b45c3aed741ffd274b72760c4674c9c28
Aanchal Jindal <aanchaljindal@gmail.com>
2017-10-15 23:56:34 -0700

    Working version with a placeholder scoring function.

--- scoreComputer.py
+++ scoreComputer.py
@@ -5,19 +5,24 @@ class ScoreComputer:
     # TODO: Use an abstract class ScoreComputer with different speciailizations
     # corresponding to different platforms (eBay, Craigslist, etc.).
     def __init__(self, listPrice):
-        # TODO: Read this values from a config file.
+        # TODO: Read these values from a config file.
         self.listPrice = listPrice
 
-    def getScore(item):
-        return __getPriceScore(item.price, item.shippingCost, self.listPrice) * __getDistanceScore(item.distance)
+    def getScore(self, item):
+        print(item.distance)
+        return self.__getPriceScore(item.price, item.shippingCost,
+                self.listPrice) * self.__getDistanceScore(item.distance)
 
-    def __getPriceScore(sellingPrice, shippingCost, listPrice):
+    def __getPriceScore(self, sellingPrice, shippingCost, listPrice):
         discount = listPrice - sellingPrice - shippingCost
         if discount < 0:
             return 0
         sigma = listPrice / 10;
-        return 1 - math.exp(- discount * discount / (2 * sigma * sigma)) / (sigma * 0.3989)
+        return 1 - math.exp(- discount * discount / (2 * sigma * sigma)) / (sigma * 2.506628)
 
-    def __getDistanceScore(distance):
-        return 1 / (1 + math.exp(distance - 500))
+    def __getDistanceScore(self, distance):
+        try:
+            return 1 / (1 + math.exp(distance - 1000))
+        except OverflowError:
+            return 0
 
